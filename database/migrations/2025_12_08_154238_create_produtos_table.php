cdd6e7ca98d3f01b9fa8e05840ce257028610e28
Pedroxox <pfelipe263@gmail.com>
2025-12-08 12:30:56 -0400

    Criação de tabelas

---
+++ database/migrations/2025_12_08_154238_create_produtos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('pedidos', function (Blueprint $table) {
+        $table->id();
+        $table->string('nome_cliente');
+        $table->string('email_cliente');
+        $table->string('telefone')->nullable();
+        $table->string('endereco_entrega');
+        $table->date('data_entrega');
+        $table->time('hora_entrega');
+        $table->decimal('valor_total', 10, 2);
+        $table->string('status')->default('aguardando_pagamento'); 
+        $table->timestamps();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
